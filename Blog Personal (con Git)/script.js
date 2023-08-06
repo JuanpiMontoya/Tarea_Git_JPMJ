1ba77226a4c2e6d811d1d6945709c0537bbc304d
unknown <juan.montoya@ucb.edu.bo>
2023-08-06 14:26:55 -0400

    Proyecto Utilizando Git Cambios 1

--- Blog Personal (con Git)/script.js
+++
@@ -1,8 +0,0 @@
-function comentarios(){
-    let favorito,opinion,comentario;
-    favorito= document.getElementById("obraGusto").value;
-    opinion = document.getElementById("opinion").value;
-    comentario = "Comentario: " + opinion +" Obra que más le intereso: " + favorito;
-    document.getElementById("Comentarios").innerHTML = comentario;
-    alert(comentario);
-}
